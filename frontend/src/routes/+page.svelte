1ed9594781db61a1bd6c1b2b22659a69a5a989e6
BrickPi <49528459+BrickPi@users.noreply.github.com>
2023-05-10 11:16:26 -0500

    Finish v1 Website Frontend
    Update README.md
    Add FAQs Page
    OpenVic2 -> OpenVic
    Add Basic News Page
    Add Static FAQs

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -3,8 +3,8 @@
 </script>
 
 <Header/>
-<a href="/news">
-    <img src="/terrain-map-4-5-23.png" alt="OpenVic2 Terrain Map Mode, Centered on Europe" width="1000"><br>
+<a href="https://storageopenvic2.blob.core.windows.net/dev-diaries/Dev_diary_4.pdf" target="_blank" rel="noopener noreferrer">
+    <img src="/terrain-map-4-5-23.png" alt="OpenVic Terrain Map Mode, Centered on Europe" width="1000"><br>
     Dev Diary IV: the Map
 </a>
 
--- frontend/src/routes/news/+page.svelte
+++ frontend/src/routes/news/+page.svelte
@@ -3,3 +3,16 @@
 </script>
 
 <Header/>
+<h1>Dev Diaries</h1>
+<a href="https://storageopenvic2.blob.core.windows.net/dev-diaries/Dev_diary_4.pdf" target="_blank" rel="noopener noreferrer">Dev Diary 4</a><br>
+<a href="https://storageopenvic2.blob.core.windows.net/dev-diaries/Dev_diary_3.pdf" target="_blank" rel="noopener noreferrer">Dev Diary 3</a><br>
+<a href="https://storageopenvic2.blob.core.windows.net/dev-diaries/Dev_diary_2.pdf" target="_blank" rel="noopener noreferrer">Dev Diary 2</a><br>
+<a href="https://storageopenvic2.blob.core.windows.net/dev-diaries/Dev_diary_1.pdf" target="_blank" rel="noopener noreferrer">Dev Diary 1</a><br>
+
+<style>
+    a {
+        font-family: 'Varela Round';
+        font-size: 3vw;
+        color: dodgerblue;
+    }
+</style>
