27d4c6141b9ff9c3e90f4260948f816e97cdb450
BrickPi <49528459+BrickPi@users.noreply.github.com>
2023-05-16 11:33:49 -0500

    Update the Logo

--- frontend/src/routes/contribute/+page.svelte
+++ frontend/src/routes/contribute/+page.svelte
@@ -7,7 +7,7 @@
 <div>
     <ul>
         <li>Join the team! If you have any experience in C++, art, networking, Godot, or design, we'd love to have you! <a href="https://forms.gle/VdFgD8W5pkDhGWQV7" target="_blank" rel="noopener noreferrer">Fill out the form</a></li>
-        <li>Test it out! If you poke around at what we've got so far, report any bugs in <a href="https://discord.com/invite/vM4E3BFkqH" target="_blank" rel="noopener noreferrer">the Discord</a> or open an issue <a href="https://www.github.com/OpenVic/Project/OpenVic" target="_blank" rel="noopener noreferrer">on GitHub</a>.</li>
+        <li>Test it out! If you poke around at what we've got so far, report any bugs in <a href="https://discord.com/invite/vM4E3BFkqH" target="_blank" rel="noopener noreferrer">the Discord</a> or open an issue <a href="https://www.github.com/OpenVicProject/OpenVic" target="_blank" rel="noopener noreferrer">on GitHub</a>.</li>
         <li>Write events and decisions. Love your LARP? Not enough flavour for your nation? Over on <a href="https://discord.com/invite/vM4E3BFkqH" target="_blank" rel="noopener noreferrer">the Discord</a> you can submit your own events and decisions.</li>
         <li>Översätt (that's Swedish for translate)! We aren't to a point where we need translators yet, but eventually we'll need your help.</li>
     </ul>
