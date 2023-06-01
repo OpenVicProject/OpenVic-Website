6b6c0bb4e0f9b264e5251d9730336016ee25d92a
BrickPi <49528459+BrickPi@users.noreply.github.com>
2023-06-01 08:46:40 -0600

    spelling fixes & UI improvements

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -4,7 +4,7 @@
 
 <Header/>
 <a href="https://openvicstorage.blob.core.windows.net/dev-diaries/Dev_diary_4.pdf" target="_blank" rel="noopener noreferrer">
-    <img src="/terrain-map-4-5-23.png" alt="OpenVic Terrain Map Mode, Centered on Europe" width="1000"><br>
+    <img id="map" src="/terrain-map-4-5-23.png" alt="OpenVic Terrain Map Mode, Centered on Europe"><br>
     Dev Diary IV: the Map
 </a>
 
@@ -14,5 +14,8 @@
     }
     a {
         font-size: 3.5vw;
+    }
+    #map {
+        width: 42vw;
     }
 </style>
--- frontend/src/routes/faqs/+page.svelte
+++ frontend/src/routes/faqs/+page.svelte
@@ -16,7 +16,7 @@
 <FAQ q={faq.question} a={faq.answer}/>
 {/each}!-->
 <FAQ q="Are you guys Open-Source?" a="Yes! Our project is licensed under GPL3."/>
-<FAQ q="What does this mean for Victoria 2 Mods?" a="We will create our own system of modding that is leagues more powerful and intuitive than PDX's current modding system for Victoria 2. However, we also plan to include full backwards compatibility for mods for Victoia 2. This means you will be able to drag your Mod files straight into OpenVic2, and they will work perfectly."/>
+<FAQ q="What does this mean for Victoria 2 Mods?" a="We will create our own system of modding that is leagues more powerful and intuitive than PDX's current modding system for Victoria 2. However, we also plan to include full backwards compatibility for mods for Victoria 2. This means you will be able to drag your Mod files straight into OpenVic, and they will work perfectly."/>
 <FAQ q="Will OpenVic be free?" a="OpenVic will be free for anyone to play, forever."/>
 <FAQ q="How much will you guys change?" a="We plan to emulate Victoria 2 as close as possible, even the quirks, although after 1.0, we will make improvements to the game that can be disabled."/>
 <FAQ q="Doesn't this violate copyright?" a="No, everything included in OpenVic is entirely created by the team or open-source."/>
