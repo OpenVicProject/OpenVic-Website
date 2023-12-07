1ba7bc27f3ec6db12bb9a59056eb78a1aa16924c
BrickPi <49528459+BrickPi@users.noreply.github.com>
2023-12-07 13:59:05 -0600

    Fix News link bug
    Signed-off-by: BrickPi <49528459+BrickPi@users.noreply.github.com>

--- frontend/src/routes/news/+page.svelte
+++ frontend/src/routes/news/+page.svelte
@@ -4,7 +4,7 @@
 
 <Header/>
 <h1>Dev Diaries</h1>
-<a href="https://openvicstorage.blob.core.windows.net/dev-diaries/Dev_Diary_6.pdf" target="_blank" rel="noopener noreferrer">Dev Diary 6</a><br>
+<a href="https://openvicstorage.blob.core.windows.net/dev-diaries/Dev_Diary_6_.pdf" target="_blank" rel="noopener noreferrer">Dev Diary 6</a><br>
 <a href="https://openvicstorage.blob.core.windows.net/dev-diaries/Dev_diary_5.pdf" target="_blank" rel="noopener noreferrer">Dev Diary 5</a><br>
 <a href="https://openvicstorage.blob.core.windows.net/dev-diaries/Dev_diary_4.pdf" target="_blank" rel="noopener noreferrer">Dev Diary 4</a><br>
 <a href="https://openvicstorage.blob.core.windows.net/dev-diaries/Dev_diary_3.pdf" target="_blank" rel="noopener noreferrer">Dev Diary 3</a><br>
